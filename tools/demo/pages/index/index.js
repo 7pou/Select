0b7f17e19402f2ab9733303ecefa6081d2b557a9
zhaoyf <604264588@qq.com>
2023-04-13 12:48:36 +0800

    feat: 添加demo示例

--- tools/demo/pages/index/index.js
+++ tools/demo/pages/index/index.js
@@ -1,99 +1,38 @@
+/**
+ * 此组件对比原生Picker解决了一下问题
+ *
+ * picker选中后返回的是range的下标, 如果在reactice-data中存选中项的index
+ * view中显示选中值 需要配置为 range[index].fieldName
+ * js中 提交数据时 也需要单独再次获取 range[index].key
+ *
+ * 使用Select组件支持model:value双向绑定的写法,也可以监听bindchange函数手动处理
+ * 使用Select可以像使用web端select组件一样,  在reactice-data存入选中项的值
+ * view中显示选中值 就交给组件来处理
+ * js中 提交数据时 可以直接取 reactice-data存入选中项的值
+ */
+
+const mapUserSex = [
+  {key: 1, value: '男'},
+  {key: 2, value: '女'},
+  {key: 3, value: '未知'},
+]
 Page({
   data: {
-    date_1: '',
-    date_2: '',
-    date_3: '',
-    date_4: ['2020-04-24 00:00', '2020-07-20 16:27'],
-    date_5: '',
-    date_6: '',
-    date_7: '',
+    mapUserSex,
+    selectValue: '',
+    selectValue2: '',
+    pickerIndex: '',
   },
 
-  // 选择日期
-  handleOpen_1() {
-    this.selectComponent('#date').open({
-      value: this.data.date_1,
-      range: false,
-      showTime: false,
-      onChange: res => {
-        this.setData({date_1: res})
-      }
-    })
+  // picker
+  handlePickerChange(e) {
+    this.setData({pickerIndex: e.detail.value})
   },
 
-  // 选择日期(带有时间)
-  handleOpen_2() {
-    this.selectComponent('#date').open({
-      value: this.data.date_2,
-      range: false,
-      onChange: res => {
-        this.setData({date_2: res})
-      }
-    })
+  // select
+  handleSelectChange(e) {
+    console.log(e.detail)
+    this.setData({selectValue: e.detail.value})
   },
 
-  // 选择日期区间
-  handleOpen_3() {
-    this.selectComponent('#date').open({
-      value: this.data.date_3,
-      showTime: false,
-      onChange: res => {
-        this.setData({date_3: res})
-      }
-    })
-  },
-  // 选择日期区间(带有时间)
-  handleOpen_4() {
-    this.selectComponent('#date').open({
-      value: this.data.date_4,
-      onChange: res => {
-        this.setData({date_4: res})
-      }
-    })
-  },
-
-  // 选择日期(自定义标题)
-  handleOpen_5() {
-    this.selectComponent('#date').open({
-      value: this.data.date_5,
-      range: false,
-      showTime: false,
-      title: '自定义的标题',
-      onChange: res => {
-        this.setData({date_5: res})
-      }
-    })
-  },
-
-  // 选择日期(自定义按钮色)
-  handleOpen_6() {
-    this.selectComponent('#date').open({
-      value: this.data.date_6,
-      range: false,
-      showTime: false,
-      confirmColor: '#36c140',
-      onChange: res => {
-        this.setData({date_6: res})
-      }
-    })
-  },
-
-  // 选择日期(自定义确定Hook)
-  handleOpen_7() {
-    this.selectComponent('#date').open({
-      value: this.data.date_7,
-      range: false,
-      showTime: false,
-      beforeHook: (e) => {
-        if (e !== '2008-08-08') {
-          wx.showToast({title: '请选择2008年8月8号', icon: 'none'})
-          return false
-        }
-        return true
-      },
-      onChange: res => {
-        this.setData({date_7: res})
-      }
-    })
-  },
 })
